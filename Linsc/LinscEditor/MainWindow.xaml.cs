c3ea481f6d7733b904e9a4e79e8f67bd1098e435
Óscar López <oscar_lm_geminis@hotmail.es>
2024-08-10 13:39:00 +0200

    Project templates integration in editor

---
+++ Linsc/LinscEditor/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using LinscEditor.GameProject;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LinscEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnMainWindowLoaded;
+        }
+
+        private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= OnMainWindowLoaded;
+            OpenProjectSelectionPage();
+        }
+
+        private void OpenProjectSelectionPage()
+        {
+            var projectSelectionPage = new ProjectSelectionPage();
+            MainFrame.Navigate(projectSelectionPage);
+        }
+    }
+}
