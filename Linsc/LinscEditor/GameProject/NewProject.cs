6653b88d7f13b29df74f3e834e10567780b4f598
Óscar López <oscar_lm_geminis@hotmail.es>
2024-08-13 16:33:25 +0200

    Data validation and error logging

--- Linsc/LinscEditor/GameProject/NewProject.cs
+++ Linsc/LinscEditor/GameProject/NewProject.cs
@@ -23,6 +23,7 @@ namespace LinscEditor.GameProject
                 if(_projectName != value)
                 {
                     _projectName = value;
+                    ValidateData();
                     OnPropertyChanged(nameof(ProjectName));
                 }
             }
@@ -37,7 +38,36 @@ namespace LinscEditor.GameProject
                 if (_projectPath != value)
                 {
                     _projectPath = value;
+                    ValidateData();
                     OnPropertyChanged(nameof(ProjectPath));
+                }
+            }
+        }
+
+        private bool _isValid = false;
+        public bool IsValid
+        {
+            get => _isValid;
+            set
+            {
+                if(_isValid != value)
+                {
+                    _isValid = value;
+                    OnPropertyChanged(nameof(IsValid));
+                }
+            }
+        }
+
+        private string _errorMsg;
+        public string ErrorMsg
+        {
+            get => _errorMsg;
+            set
+            {
+                if(_errorMsg != value)
+                {
+                    _errorMsg = value;
+                    OnPropertyChanged(nameof(ErrorMsg));
                 }
             }
         }
@@ -65,14 +95,49 @@ namespace LinscEditor.GameProject
                 }
 
                 ProjectTemplates = new(_projectTemplates);
+                ValidateData();
             }
             catch (Exception ex)
             {
                 //TODO: properly show an error dialog window
                 Debug.WriteLine(ex.Message);
             }
+        }
 
+        private void ValidateData()
+        {
+            if (!Path.EndsInDirectorySeparator(ProjectPath))
+            {
+                ProjectPath += @"\";
+            }
 
+            IsValid = false;
+            if (string.IsNullOrWhiteSpace(ProjectName.Trim()))
+            {
+                ErrorMsg = "The name of the project cannot be empty.";
+            }
+            else if(ProjectName.IndexOfAny(Path.GetInvalidFileNameChars()) != -1)
+            {
+                ErrorMsg = "The project name constains invalid characters.";
+            }
+            //TODO: think on deleting this. The path is never empty since the path gets a "\" added at the beginning of the method
+            else if(string.IsNullOrWhiteSpace(ProjectPath.Trim()))
+            {
+                ErrorMsg = "The path of the project cannot be empty.";
+            }
+            else if (ProjectPath.IndexOfAny(Path.GetInvalidPathChars()) != -1)
+            {
+                ErrorMsg = "The path of the project contains invalid characters.";
+            }
+            else if(Directory.Exists(ProjectPath) && Directory.EnumerateFileSystemEntries(ProjectPath).Any())
+            {
+                ErrorMsg = "The selected directory already exists and is not empty.";
+            }
+            else
+            {
+                IsValid = true;
+                ErrorMsg = string.Empty;
+            }
         }
     }
 }
