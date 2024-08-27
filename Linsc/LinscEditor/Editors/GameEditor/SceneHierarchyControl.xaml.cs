e483e6d896d739c9a28b407314a9cc67f30efbd9
Óscar López <oscar_lm_geminis@hotmail.es>
2024-08-27 11:48:10 +0200

    Controls to add and remove scenes
    This controls are making use of the undoredo system

--- Linsc/LinscEditor/Editors/GameEditor/SceneHierarchyControl.xaml.cs
+++ Linsc/LinscEditor/Editors/GameEditor/SceneHierarchyControl.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using LinscEditor.GameProject;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LinscEditor.Editors.GameEditor
 {
@@ -24,12 +13,6 @@ namespace LinscEditor.Editors.GameEditor
         public SceneHierarchyControl()
         {
             InitializeComponent();
-        }
-
-        private void OnAddSceneButton_Click(object sender, RoutedEventArgs e)
-        {
-            var project = DataContext as Project;
-            project.AddScene("New Scene " + project.Scenes.Count);
         }
     }
 }
