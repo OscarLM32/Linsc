edf3968f9df4fecd05eb841e6e1843f9fdb98ea3
Óscar López <oscar_lm_geminis@hotmail.es>
2024-09-02 12:46:50 +0200

    New TextBox controls
    When pressing enter the changes to a textbox are confirmed and when pressing escaped they are ignored

---
+++ Linsc/LinscEditor/Dictionaries/ControlTemplates.xaml.cs
@@ -0,0 +1,36 @@
+﻿using LinscEditor.Extensions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace LinscEditor.Dictionaries
+{
+    public partial class ControlTemplates : ResourceDictionary
+    {
+        private void OnTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            var exp = textBox.GetBindingExpression(TextBox.TextProperty);
+            if (exp == null) return;
+
+            if (e.Key == Key.Enter)
+            {
+                ICommand command = textBox.GetCommand();
+
+                if(command != null && command.CanExecute(textBox.Text))
+                {
+                    command.Execute(textBox.Text);
+                }
+                else
+                {
+                    exp.UpdateSource();
+                }
+            }
+            else if (e.Key == Key.Escape)
+            {
+                exp.UpdateTarget();
+                Keyboard.ClearFocus();
+            }
+        }
+    }
+}
---
+++ Linsc/LinscEditor/Helpers/TextBoxHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace LinscEditor.Helpers
+{
+    public static class TextBoxHelper
+    {
+        public static readonly DependencyProperty Command =
+            DependencyProperty.RegisterAttached
+            (
+                "Command",
+                typeof(ICommand),
+                typeof(TextBoxHelper),
+                new PropertyMetadata(null)
+            );
+
+        public static ICommand GetCommand(DependencyObject obj)
+        {
+            return (ICommand)obj.GetValue(Command);
+        }
+
+        public static void SetCommand(DependencyObject obj, ICommand value)
+        {
+            obj.SetValue(Command, value);
+        }
+    }
+}
