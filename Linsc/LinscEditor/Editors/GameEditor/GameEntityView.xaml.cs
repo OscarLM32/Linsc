95f575574b1fa3211cfbb38d3f701a841dd74286
Óscar López <oscar_lm_geminis@hotmail.es>
2024-09-12 15:49:38 +0200

    New rename and isEnabled commands

--- Linsc/LinscEditor/Editors/GameEditor/GameEntityView.xaml.cs
+++ Linsc/LinscEditor/Editors/GameEditor/GameEntityView.xaml.cs
@@ -1,9 +1,15 @@
-﻿using System.Windows.Controls;
+﻿using LinscEditor.Components;
+using LinscEditor.GameProject;
+using LinscEditor.Utilities;
+using System.Windows.Controls;
 
 namespace LinscEditor.Editors.GameEditor
 {
     public partial class GameEntityView : UserControl
     {
+        private Action _undoAction;
+        private string _propertyName;
+
         public static GameEntityView Instance { get; private set; }
 
         public GameEntityView()
@@ -11,6 +17,61 @@ namespace LinscEditor.Editors.GameEditor
             InitializeComponent();
             DataContext = null;
             Instance = this;
+
+            DataContextChanged += (_, __) =>
+            {
+                if (DataContext != null)
+                {
+                    (DataContext as MSEntity).PropertyChanged += (s, e) => _propertyName = e.PropertyName;
+                }
+            };
+        }
+
+        private void OnName_TextBox_GotKeyboardFocus(object sender, System.Windows.Input.KeyboardFocusChangedEventArgs e)
+        {
+            _undoAction = GetRenameAction();
+        }
+
+        private void OnName_TextBox_LostKeyboardFocus(object sender, System.Windows.Input.KeyboardFocusChangedEventArgs e)
+        {
+            if(_propertyName == nameof(MSEntity.Name) && _undoAction != null)
+            {
+                var redoAction = GetRenameAction();
+                Project.UndoRedo.Add(new UndoRedoAction(_undoAction, redoAction, "Rename game entity"));
+                _propertyName = null;
+            }
+            _undoAction = null;
+        }
+
+        private Action GetRenameAction()
+        {
+            var vm = DataContext as MSEntity;
+            var selection = vm.SelectedEntities.Select(entity => (entity, entity.Name)).ToList();
+            return new Action(() =>
+            {
+                selection.ForEach(item => item.entity.Name = item.Name);
+                (DataContext as MSEntity).Refresh();
+            });
+        }
+
+        private void OnIsEnabled_CheckBox_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var undoAction = GetIsEnabledAction();
+            var vm = DataContext as MSEntity;
+            vm.IsEnabled = (sender as CheckBox).IsChecked == true;
+            var redoAction = GetIsEnabledAction();
+            Project.UndoRedo.Add(new UndoRedoAction(undoAction, redoAction, vm.IsEnabled == true ? "Enable items" : "Disable items"));
+        }
+
+        private Action GetIsEnabledAction()
+        {
+            var vm = DataContext as MSEntity;
+            var selection = vm.SelectedEntities.Select(entity => (entity, entity.IsEnabled)).ToList();
+            return new Action(() =>
+            {
+                selection.ForEach(item => item.entity.IsEnabled = item.IsEnabled);
+                (DataContext as MSEntity).Refresh();
+            });
         }
     }
 }
